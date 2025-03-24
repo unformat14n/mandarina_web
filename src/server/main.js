59556ae46eca8289254cea4c58dc63812ed1e27e
TheKennyman07 <69918528+KennyMan14@users.noreply.github.com>
2025-03-24 11:30:19 -0600

    Attempt to fix fetch requests

--- src/server/main.js
+++ src/server/main.js
@@ -4,7 +4,7 @@ import mysql from "mysql2";
 import cors from "cors"; // Import CORS module
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 // app.use(cors()); // Enable CORS for all domains
 
