5cd1b543d864e1648dbf4b221f0d34fb7b7a18ae
TheKennyman07 <69918528+KennyMan14@users.noreply.github.com>
2025-03-23 22:25:28 -0600

    Change fetch requests

--- src/client/components/Register.jsx
+++ src/client/components/Register.jsx
@@ -14,7 +14,7 @@ function Register() {
         }
 
         try {
-            const response = await fetch('http://localhost:4000/register', {
+            const response = await fetch('/register', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ username, password })
